e3dc3ed09de5dc707f8a6eb9f367c5e07197d1a5
tibeal <tibeal@gmail.com>
2018-04-10 15:10:51 -0300

    Fix login issues

--- src/aura/Gallery/GalleryController.js
+++ src/aura/Gallery/GalleryController.js
@@ -5,22 +5,30 @@
         action.setCallback(this, function(response){
             var state = response.getState();
             if (state === 'SUCCESS') {
-                component.set('c.ssoReady',response.getReturnValue());
+                component.set('v.ssoReady',response.getReturnValue());
+            } else if (state === 'ERROR') {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
             }
         });
+        $A.enqueueAction(action);
 	},
     updateMember: function(component, event, helper){
         var newMember = event.getParam('member');
         // Create the action
         var action = component.get("c.setCurrentMember");        
         if (newMember) {
-	        action.setParams({ member : newMember });            
+	        action.setParams({ member : newMember });
         }
         // Add callback behavior for when response is received
         action.setCallback(this, function(response) {
             var state = response.getState();
-            console.log(state);
-            console.log(component.isValid());
             var member = response.getReturnValue();
             if (component.isValid() && state === "SUCCESS") {
                 var refreshView = false;
@@ -31,12 +39,18 @@
                 if (refreshView) {
                     $A.get("e.force:refreshView").fire();
                 }
-            }
-            else {
-                console.log("Failed with state: " + state);
+            } else if (state === 'ERROR') {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + 
+                                 errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
             }
         });    
-        // Send action off to be executed
         $A.enqueueAction(action);
 	}
 })
